e5236ea8c000937d503d727d06531263dcc17592
riyasoni5990 <47269531+riyasoni5990@users.noreply.github.com>
2023-08-18 10:52:10 +0530

    Added Assignment-1's answer

---
+++ Assignment-1/answer.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mumbai_ec2"
+  }
+}
